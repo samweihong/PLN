6cd88939b4954a4f0eff0c31dd204380163d3888
Sam Wei Hong <weihongsam2002@gmail.com>
2022-03-21 16:06:01 +0800

    :bulb: Add explanation to PLN2022 Closed Preliminary B

--- src/PLN2022/closed_preliminary/B.java
+++ src/PLN2022/closed_preliminary/B.java
@@ -5,6 +5,24 @@ import java.util.Scanner;
 public class B {
     static Scanner sc = new Scanner(System.in);
 
+    /**
+     * Determine if the card holder can fit all cards up to that point.
+     *
+     * Intuition
+     * A card holder can fit all cards, is equivalent to
+     * Its (width/height) is not smaller than the width of all cards, and
+     * its (height/width) is not smaller than the height of all cards.
+     *
+     * This is again, equivalent to,
+     * Its (width/height) is not smaller than the maximum width of all cards, and
+     * its (height/width) is not smaller than the maximum height of all cards.
+     *
+     * Also, we consider the width of a card should always be its longer side to minimize the space required.
+     * Imagine a (1 cm x 100 cm) card and another (100 cm x 1 cm) card,
+     * the 100 cm should be taken as the width while the 1 cm should be taken as the height
+     * so that the maximum width is 100 cm and the maximum height is 1 cm.
+     * Otherwise, both the maximum width and height will be 100 cm.
+     */
     public static void main(String[] args) {
         int n = sc.nextInt();
         int maxW = 0;
