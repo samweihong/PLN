7cb37fc9d66ba702f62088a896585cbd96af1863
Sam Wei Hong <weihongsam2002@gmail.com>
2022-03-21 15:46:32 +0800

    :art: Optimize PLN2022 Closed Preliminary B

--- src/PLN2022/closed_preliminary/B.java
+++ src/PLN2022/closed_preliminary/B.java
@@ -5,37 +5,24 @@ import java.util.Scanner;
 public class B {
     static Scanner sc = new Scanner(System.in);
 
-    public static void solution() {
+    public static void main(String[] args) {
         int n = sc.nextInt();
         int maxW = 0;
         int maxH = 0;
         for (int i = 0; i < n; i++) {
-            String c = sc.next();
-            if (c.equals("+")) {
+            if (sc.next().equals("+")) {
                 int w = sc.nextInt();
                 int h = sc.nextInt();
-                if (w > h) {
-                    maxW = Math.max(maxW, w);
-                    maxH = Math.max(maxH, h);
-                } else {
-                    maxW = Math.max(maxW, h);
-                    maxH = Math.max(maxH, w);
-                }
-                continue;
-            }
-            if (c.equals("?")) {
+                maxW = Math.max(maxW, Math.max(w, h));
+                maxH = Math.max(maxH, Math.min(w, h));
+            } else {
                 int queryW = sc.nextInt();
                 int queryH = sc.nextInt();
-                if (maxW <= queryW && maxH <= queryH ||
-                    maxH <= queryW && maxW <= queryH)
+                if (maxW <= Math.max(queryW, queryH) && maxH <= Math.min(queryW, queryH))
                     System.out.println("1");
                 else
                     System.out.println("0");
             }
         }
-    }
-
-    public static void main(String[] args) {
-        solution();
     }
 }
